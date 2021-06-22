0340367f480e6042a874cba7efa093a8b40d0c7c
Vikas-Singh-IITR <vsingh@ar.iitr.ac.in>
2021-06-22 22:32:06 +0530

    populated dynamic data from the api

---
+++ script.js
@@ -0,0 +1,37 @@
+const quoteContainer = document.getElementById("quoteContainer");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("newQuote");
+
+let apiQuotes = [];
+
+//show quote
+function newQuote() {
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  if (!quote.author) {
+    authorText.textContent = "Unknow";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  if (quote.text.length > 100) {
+    quoteText.classList.add("longQuote");
+  } else {
+    quoteText.classList.remove("longQuote");
+  }
+  quoteText.textContent = quote.text;
+}
+
+//fetching quotes from the API
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    //Catch error here
+  }
+}
+
+getQuotes();
