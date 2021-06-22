6d87d906764215306e6983c1e71c989fb32d537c
Vikas-Singh-IITR <vsingh@ar.iitr.ac.in>
2021-06-22 23:11:23 +0530

    created and itegrated loader

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitterButton");
 const newQuoteBtn = document.getElementById("newQuote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+//show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+//hide loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 //show quote
 function newQuote() {
+  loading();
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   if (!quote.author) {
     authorText.textContent = "Unknow";
@@ -20,10 +34,12 @@ function newQuote() {
     quoteText.classList.remove("longQuote");
   }
   quoteText.textContent = quote.text;
+  complete();
 }
 
 //fetching quotes from the API
 async function getQuotes() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
